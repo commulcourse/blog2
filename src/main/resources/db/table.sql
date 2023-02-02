b1e3ff13183b350cd1c5480ce3d2234eb54faf26
commulcourse <commulcourse@gmail.com>
2023-02-02 16:35:15 +0900

    프로젝트연결

---
+++ src/main/resources/db/table.sql
@@ -0,0 +1,4 @@
+insert into user_tb(username, password, email, created_at)
+
+
+insert into board_tb(title, user_id, created_at)
---
+++ src/test/java/shop/mtcoding/blog/BlogApplicationTests.java
@@ -0,0 +1,8 @@
+package shop.mtcoding.blog;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+}
